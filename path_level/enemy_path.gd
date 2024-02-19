60dfff15a9fb5cb7426b191c6b742104e4f4a67c
Mat-A2k <matthias15433@gmail.com>
2024-02-19 11:27:56 -0500

    added ability to spawn enemies on different rows in path level

--- path_level/enemy_path.gd
+++ path_level/enemy_path.gd
@@ -1,20 +1,42 @@
+@tool
 extends Path3D
 
+@export var path_seed = 0
 @export var wander_scale = 0.5
+@export var row_x_value = 0.0
+@export var row_count = 4
+@export var road_width = 1.0
+var enemies = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 var x = 0.0
+var rand = 0.0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	$PathFollow3D.progress += delta
-	x = $PathFollow3D.progress
-	$PathFollow3D/Node3D.position = Vector3(wander_scale*
-		sin(x)/10 +
-		sin(3.5*x)/20,
-		0, 0)
-	$PathFollow3D/Node3D/MeshInstance3D.rotation_degrees = Vector3(0,
-		((cos(x)/10) + (3.5*cos(3.5*x)/20))*-110*wander_scale,
-		0)
+func _process(delta, row_x_value = row_x_value, road_width=road_width, row_count=row_count):
+	for e in range(len(enemies)):
+		enemies[e].progress += delta
+		
+		row_count = 4
+		road_width = 1.0
+		
+		rand = ((1+e) % row_count)/2.0 - 1.0
+		x = enemies[e].progress + e**2
+		enemies[e].get_child(0).position = Vector3(rand + (wander_scale*
+			sin(x)/10 +
+			sin(3.5*x)/20),
+			0, 0)
+		
+		enemies[e].get_child(0).rotation_degrees = Vector3(0,
+			((cos(x)/10) + (3.5*cos(3.5*x)/20))*-114.59*wander_scale,
+			0)
+
+
+func _on_enemy_pressed():
+	var enemy_controller = PathFollow3D.new()
+	var enemy = preload('res://path_level/enemy.tscn').instantiate()
+	enemy_controller.add_child(enemy)
+	add_child(enemy_controller)
+	enemies.append(enemy_controller)
--- path_level/path_level.gd
+++ path_level/path_level.gd
@@ -1,16 +1,18 @@
 extends Node3D
 
+@onready var enemy_path = $enemy_path
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+@export var camera_view = true
 const camera_speed = 0.005
 @export var direction = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$camera_path/PathFollow3D.progress_ratio += direction*camera_speed
+	$camera_path/close_up_view.progress_ratio += direction*camera_speed
 	$enemy_path/camera_follow.progress_ratio += direction*camera_speed
 
 func _on_left_button_down():
@@ -24,3 +26,18 @@ func _on_right_button_down():
 
 func _on_right_button_up():
 	direction += 1
+
+func _on_toggle_view_pressed():
+	#if(camera_view):
+		#var camera = $camera_path/close_up_view/camera
+		#camera.look_at_Mode('None')
+		#camera.get_parent().remove_child(camera)
+		#$top_view.add_child(camera)
+		#$camera_path/camera_move.visible = false
+	#else:
+		#var camera = $camera_path/close_up_view/camera
+		#camera.get_parent().remove_child(camera)
+		#$camera_path/close_up_view/camera.add_child(camera)
+		#$camera_path/camera_move.visible = true
+	#camera_view = !camera_view
+	pass
